2b94a3070ac8acaf5460ee37611eb445be71bb1b
cloudcome <cloudcome@163.com>
2016-01-26 21:59:21 +0800

    coolie-demo10

---
+++ webroot-dev/static/js/app/year-month-date.js
@@ -0,0 +1,7 @@
+define(function (require, exports, module) {
+    var year = require('../libs/year.js');
+    var month = require('../libs/month.js');
+    var date = require('../libs/date.js');
+
+    alert('today is ' + year() + '-' + month() + '-' + date());
+});
---
+++ webroot-dev/static/js/app/year-month.js
@@ -0,0 +1,6 @@
+define(function (require, exports, module) {
+    var year = require('../libs/year.js');
+    var month = require('../libs/month.js');
+
+    alert('today is ' + year() + '-' + month());
+});
